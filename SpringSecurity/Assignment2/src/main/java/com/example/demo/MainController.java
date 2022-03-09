8207c575e0ebdd9f21e8d312ebd9597b42bf702a
rahul3183 <rahullama34@gmail.com>
2022-03-09 10:17:41 +0530

    A2

---
+++ SpringSecurity/Assignment2/src/main/java/com/example/demo/MainController.java
@@ -0,0 +1,39 @@
+package com.example.demo;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    @GetMapping("/")
+    public String Home(){
+        return "Welcome";
+    }
+
+    @GetMapping("/user")
+    public  String getUser(){
+        return "Welcome user";
+    }
+
+    @GetMapping("/admin")
+    public String getAdmin(){
+        return "Welcome admin";
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String login(Model model, String error, String logout) {
+        if (error != null)
+            model.addAttribute("errorMsg", "Your username and password are invalid.");
+
+        if (logout != null)
+            model.addAttribute("msg", "You have been logged out successfully.");
+
+        return "login";
+    }
+
+
+}
---
+++ SpringSecurity/Assignment2/src/main/java/com/example/demo/WebSecurity.java
@@ -0,0 +1,42 @@
+package com.example.demo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+
+
+@EnableWebSecurity
+public class WebSecurity  extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user")
+                .password("user123")
+                .roles("user");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/admin").hasRole("ADMIN")
+                .antMatchers("/user").hasAnyRole("USER", "ADMIN")
+                .antMatchers("/").permitAll()
+                .and().formLogin().loginPage("/login")
+                .failureUrl("/login.jsp?error=1").loginProcessingUrl("/login")
+                .permitAll().and().logout();
+
+        http.csrf().disable();
+    }
+    @Bean
+    public PasswordEncoder getPasswordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+
+}
